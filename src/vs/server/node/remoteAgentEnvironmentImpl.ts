tonmoykhan111/vscode

cli: provide option for unsupported glibc sysroot

src/vs/server/node/remoteAgentEnvironmentImpl.ts
@@ -103,7 +103,7 @@ export class RemoteAgentEnvironmentChannel implements IServerChannel {
 		if (process.platform === 'linux') {
 			const glibcVersion = (process as ProcessWithGlibc).glibcVersion;
 			const minorVersion = glibcVersion ? parseInt(glibcVersion.split('.')[1]) : 28;
-			isUnsupportedGlibc = (minorVersion <= 27);
+			isUnsupportedGlibc = (minorVersion <= 27) || !!process.env['VSCODE_CUSTOM_GLIBC_LINKER'];
 		}
 		return {
 			pid: process.pid,

srikanthonl/codewithme

src/vs/server/node/remoteAgentEnvironmentImpl.ts
@@ -103,7 +103,7 @@ export class RemoteAgentEnvironmentChannel implements IServerChannel {
 		if (process.platform === 'linux') {
 			const glibcVersion = (process as ProcessWithGlibc).glibcVersion;
 			const minorVersion = glibcVersion ? parseInt(glibcVersion.split('.')[1]) : 28;
-			isUnsupportedGlibc = (minorVersion <= 27);
+			isUnsupportedGlibc = (minorVersion <= 27) || !!process.env['VSCODE_SERVER_CUSTOM_GLIBC_LINKER'];
 		}
 		return {
 			pid: process.pid,